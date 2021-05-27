f7471acdfa6f10ccceb11a61ef51baa3e6517f21
ELIF <ELIF@ELIF>
2021-05-27 03:02:03 +0300

    kamp6Odev3

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/concretes/ProductManager.java
@@ -2,10 +2,15 @@ package kodlamaio.northwind.dataAccess.concretes;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
@@ -22,9 +27,19 @@ public class ProductManager implements ProductService {
 	}
 
 	@Override
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 		
-		return this.productDao.findAll();
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data listelendi");
+		
 	}
 
-}
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün eklendi");
+	}
+
+	
+	
+	}
+
